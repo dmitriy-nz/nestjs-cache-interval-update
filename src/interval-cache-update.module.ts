5f2e85ccff041168347029829854cd6d6e922916
eggplant <diimonss@gmail.com>
2020-10-14 19:53:26 +0300

    - types fix;

--- src/interval-cache-update.module.ts
+++ src/interval-cache-update.module.ts
@@ -1,41 +1,38 @@
-import { DynamicModule, Module } from '@nestjs/common';
-import { MetadataScanner } from '@nestjs/core';
-import { CacheIntervalUpdateService } from './services/cache-interval-update.service';
-import { CacheOptions, StoreConfig } from 'cache-manager';
-import { DefaultCacheConfig } from './config/DefaultCache.config';
-import { OPTIONS_PROVIDE_KEY } from './config/OptionsProvideKey.config';
-import { IntervalUpdateCacheInterceptor } from './interceptors/IntervalUpdateCache.interceptor';
+import {DynamicModule, Module} from '@nestjs/common';
+import {MetadataScanner} from '@nestjs/core';
+import {CacheIntervalUpdateService} from './services/cache-interval-update.service';
+import {DefaultCacheConfig, IntervalCacheUpdateConfig} from './config/DefaultCache.config';
+import {OPTIONS_PROVIDE_KEY} from './config/OptionsProvideKey.config';
 
 
 @Module({
-  providers: [
-    CacheIntervalUpdateService,
-    MetadataScanner,
-    //IntervalUpdateCacheInterceptor,
-    {
-      provide: OPTIONS_PROVIDE_KEY,
-      useValue: DefaultCacheConfig,
-    },
-  ],
-  exports: [CacheIntervalUpdateService],
+    providers: [
+        CacheIntervalUpdateService,
+        MetadataScanner,
+        {
+            provide: OPTIONS_PROVIDE_KEY,
+            useValue: DefaultCacheConfig,
+        },
+    ],
+    exports: [CacheIntervalUpdateService],
 })
 export class IntervalCacheUpdateModule {
 
-  constructor(private c: CacheIntervalUpdateService) {
+    constructor(private c: CacheIntervalUpdateService) {
 
-  }
+    }
 
-  static register(options: StoreConfig & CacheOptions): DynamicModule {
-    return {
-      module: IntervalCacheUpdateModule,
-      providers: [
-        {
-          provide: OPTIONS_PROVIDE_KEY,
-          useValue: options,
-        },
-      ],
-    };
-  }
+    static register(options: IntervalCacheUpdateConfig): DynamicModule {
+        return {
+            module: IntervalCacheUpdateModule,
+            providers: [
+                {
+                    provide: OPTIONS_PROVIDE_KEY,
+                    useValue: options,
+                },
+            ],
+        };
+    }
 
 
 }
